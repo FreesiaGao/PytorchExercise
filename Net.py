fa2750b872406f2ac4c78a8f84c6ab4b01f59aea
FreesiaGao <1014398334@qq.com>
2018-05-07 09:52:56 +0800

    modify

--- Net.py
+++ Net.py
@@ -2,13 +2,15 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
 
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         # input channels, output channels, kernel
-        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv1 = nn.Conv2d(3, 6, 5)
         self.conv2 = nn.Conv2d(6, 16, 5)
         # input neural, output neural
         self.fc1 = nn.Linear(16*5*5, 120)
@@ -19,43 +21,56 @@ class Net(nn.Module):
         x = F.max_pool2d(F.relu(self.conv1(x)), 2)
         x = F.max_pool2d(F.relu(self.conv2(x)), 2)
         # flat the tensor, output a 2d tensor, -1 means it depends on the other dimension
-        x = x.view(-1, self.num_flat_features(x))
+        x = x.view(-1, 16*5*5)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
 
-    def num_flat_features(self, x):
-        size = x.size()[1:]
-        num_features = 1
-        for s in size:
-            num_features *= s
-        return num_features
 
+if __name__ == '__main__':
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-net = Net()
-input = torch.randn(1, 1, 32, 32)
-output = net(input)
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=False, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                              shuffle=True, num_workers=2)
 
-target = torch.arange(1, 11)
-target = target.view(1, -1)
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                           download=False, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                             shuffle=False, num_workers=2)
 
-criterion = nn.MSELoss()
-optimizer = optim.SGD(net.parameters(), lr=0.01)
+    classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
+    net = Net()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-print('conv1.bias.grad before backward')
-print(net.conv1.bias.data)
+    for epoch in range(2):
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the input
+            inputs, labels = data
 
-# in training loop:
-# zero the gradient buffers
-optimizer.zero_grad()
-output = net(input)
-loss = criterion(output, target)
-loss.backward()
-# does the update
-optimizer.step()
+            # zero the parameter gradients
+            optimizer.zero_grad()
 
-print('conv1.bias.grad after backward')
-print(net.conv1.bias.data)
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+
+    print('Finished Training')
 

ff221b5ef42db14e5799f3ca94cbef78dfe1ef0f
FreesiaGao <1014398334@qq.com>
2018-05-10 07:37:26 +0800

    lower torch version with gpu test

--- mnist.py
+++ mnist.py
@@ -1,25 +1,113 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+from torch.autograd import Variable
+import torch.utils.data as Data
+import torchvision
+import time
+
+torch.manual_seed(1)    # reproducible
 
 
-class MNISTConv(nn.Module):
-
+class CNN(nn.Module):
     def __init__(self):
-        super(MNISTConv, self).__init__()
-        # 1 * 28 * 28 size images
-        self.conv1 = nn.Conv2d(1, 10, 5)
-        self.pool1 = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(10, 20, 5)
-        self.pool2 = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
+        super(CNN, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=16,
+                kernel_size=5,
+                stride=1,
+                padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        )
+        self.conv2 = nn.Sequential(  # input shape (16, 14, 14)
+            nn.Conv2d(16, 32, 5, 1, 2),  # output shape (32, 14, 14)
+            nn.ReLU(),  # activation
+            nn.MaxPool2d(2),  # output shape (32, 7, 7)
+        )
+        self.out = nn.Linear(32 * 7 * 7, 10)   # fully connected layer, output 10 classes
 
     def forward(self, x):
-        x = self.pool1(F.relu(self.conv1(x)))
-        x = self.pool2(F.relu(self.conv2(x)))
+        x = self.conv1(x)
+        x = self.conv2(x)
         x = x.view(x.size(0), -1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        return x
+        output = self.out(x)
+        return output
 
+
+# Hyper Parameters
+EPOCH = 1
+BATCH_SIZE = 50
+LR = 0.001
+DOWNLOAD_MNIST = True
+
+
+# Mnist
+train_data = torchvision.datasets.MNIST(
+    root='./mnist/',
+    train=True,  # this is training data
+    transform=torchvision.transforms.ToTensor(),
+    download=DOWNLOAD_MNIST
+)
+
+test_data = torchvision.datasets.MNIST(root='./mnist/', train=False)
+
+train_loader = Data.DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
+
+test_x = Variable(torch.unsqueeze(test_data.test_data, dim=1), volatile=True).type(torch.FloatTensor)[:2000]/255.   # shape from (2000, 28, 28) to (2000, 1, 28, 28), value in range(0,1)
+test_y = test_data.test_labels[:2000]
+
+cnn = CNN()
+optimizer = torch.optim.Adam(cnn.parameters(), lr=LR)   # optimize all cnn parameters
+loss_func = nn.CrossEntropyLoss()   # the target label is not one-hotted
+
+start = time.clock()
+
+# training and testing
+for epoch in range(EPOCH):
+    for step, (x, y) in enumerate(train_loader):
+        b_x = Variable(x)   # batch x
+        b_y = Variable(y)   # batch y
+
+        output = cnn(b_x)               # cnn output
+        loss = loss_func(output, b_y)   # cross entropy loss
+        optimizer.zero_grad()           # clear gradients for this training step
+        loss.backward()                 # backpropagation, compute gradients
+        optimizer.step()                # apply gradients
+        print("| EPOCK:", EPOCH, "| STEP:", step)
+
+end = time.clock()
+
+cpu_time = end - start
+
+cnn1 = CNN()
+cnn1.cuda()
+
+start = time.clock()
+# training and testing
+for epoch in range(EPOCH):
+    for step, (x, y) in enumerate(train_loader):
+        b_x = Variable(x)   # batch x
+        b_x.cuda()
+        b_y = Variable(y)   # batch y
+        b_y.cuda()
+
+        output = cnn(b_x)               # cnn output
+        loss = loss_func(output, b_y)   # cross entropy loss
+        optimizer.zero_grad()           # clear gradients for this training step
+        loss.backward()                 # backpropagation, compute gradients
+        optimizer.step()                # apply gradients
+        print("| EPOCK:", EPOCH, "| STEP:", step)
+
+end = time.clock()
+
+gpu_time = end - start
+print("cpu time:", cpu_time)
+print("gpu time:", gpu_time)
+
+# test_output = cnn(test_x[:10])
+# pred_y = torch.max(test_output, 1)[1].data.numpy().squeeze()
+# print(pred_y, 'prediction number')
+# print(test_y[:10].numpy(), 'real number')
